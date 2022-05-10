881127555a7c9ff4827b85d13718370020fcb6aa
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2022-05-10 15:48:55 +0300

    adding development/production variables in config file

--- config/config.js
+++ config/config.js
@@ -1,6 +1,16 @@
+const env = process.env.PORT;
+
 const config = {
-    PORT: process.env.PORT || 3030,
-    CONNECTION_STRING: 'mongodb://localhost:27017/my-cookbook',
+    development: {
+        PORT: 3030,
+        CONNECTION_STRING: 'mongodb://localhost:27017/my-cookbook',
+    },
+    production: {
+        PORT: process.env.PORT,
+        // CONNECTION_STRING: process.env.CONNECTION_STRING,
+        CONNECTION_STRING:
+            'mongodb+srv://wickedlolz1:wickedlolz1@cluster0.bwehj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    },
 };
 
-module.exports = config;
+module.exports = env ? config.production : config.development;
