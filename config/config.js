e64d494b48c71571b35899af4b62762d38bd9b6b
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2022-05-26 21:21:11 +0300

    add prod db string

--- config/config.js
+++ config/config.js
@@ -7,9 +7,7 @@ const config = {
     },
     production: {
         PORT: process.env.PORT,
-        // CONNECTION_STRING: process.env.CONNECTION_STRING,
-        CONNECTION_STRING:
-            'mongodb+srv://wickedlolz1:wickedlolz1@cluster0.bwehj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        CONNECTION_STRING: process.env.CONNECTION_STRING,
     },
 };
 
