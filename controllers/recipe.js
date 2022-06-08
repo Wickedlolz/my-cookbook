cc48c88499142d88286dd2fec080685486a8cf13
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-08 11:11:02 +0300

    refactoring controllers, combine all CRUD in one

---
+++ controllers/recipe.js
@@ -0,0 +1,179 @@
+const router = require('express').Router();
+
+const recipeService = require('../services/recipe');
+const commentService = require('../services/comment');
+
+const { body, validationResult } = require('express-validator');
+const mapErrors = require('../util/mapper');
+
+router.get('/', async (req, res) => {
+    const recipes = await recipeService.getAll(req.query);
+
+    res.render('catalog', { recipes, query: req.query.search });
+});
+
+router.get('/details/:id', async (req, res) => {
+    const recipe = await recipeService.getOneById(req.params.id);
+    let comments = await commentService.getCommentsForRecipeById(req.params.id);
+
+    if (req.session.user && req.session.user.id == recipe.author) {
+        res.locals.isOwner = true;
+    }
+
+    // if (req.session.user) {
+    //     res.locals.isLogged = true;
+    // }
+
+    res.render('details', { recipe, comments });
+});
+
+router.post('/details/:id', async (req, res) => {
+    const content = req.body.content;
+
+    if (req.session.user) {
+        try {
+            if (content == '') {
+                throw new Error('Comment fields are required!');
+            }
+
+            await commentService.create(
+                req.params.id,
+                req.session.user.id,
+                content
+            );
+
+            res.redirect('/recipes/details/' + req.params.id);
+        } catch (error) {
+            console.error(error.message);
+            res.redirect('/details/' + req.params.id);
+        }
+    } else {
+        res.redirect('/users/login');
+    }
+});
+
+router.get('/create', (req, res) => {
+    res.render('create');
+});
+
+router.post(
+    '/create',
+    body('name').trim(),
+    body('img').trim(),
+    body('ingredients').trim(),
+    body('steps').trim(),
+    body('name')
+        .notEmpty()
+        .withMessage('Recipe name is required!')
+        .bail()
+        .isLength({ min: 3 })
+        .withMessage('Recipe name must be at least 3 characters long!'),
+    body('img')
+        .notEmpty()
+        .withMessage('Image URL is required!')
+        .bail()
+        .custom(
+            (value, { req }) =>
+                value.startsWith('http') || value.startsWith('https')
+        )
+        .withMessage('Invalid Image URL'),
+    body('ingredients').notEmpty().withMessage('Ingredients is required!'),
+    body('steps').notEmpty().withMessage('Steps is required!'),
+    async (req, res) => {
+        const { errors } = validationResult(req);
+        let { name, img, ingredients, steps } = req.body;
+        ingredients = ingredients.split('\r\n');
+        steps = steps.split('\r\n');
+
+        try {
+            if (errors.length > 0) {
+                throw errors;
+            }
+
+            await recipeService.create({
+                name,
+                imageUrl: img,
+                ingredients,
+                steps,
+                author: req.session.user.id,
+            });
+
+            res.redirect('/');
+        } catch (error) {
+            const errors = mapErrors(error);
+            const data = { name, img, ingredients, steps };
+            res.render('create', { errors, data });
+        }
+    }
+);
+
+router.get('/edit/:id', async (req, res) => {
+    const recipeId = req.params.id;
+    const recipe = await recipeService.getOneById(recipeId);
+
+    if (recipe.author != req.session.user.id) {
+        return res.redirect('/users/login');
+    }
+
+    recipe.ingredients = recipe.ingredients.join('\r\n');
+    recipe.steps = recipe.steps.join('\r\n');
+
+    res.render('edit', { recipe });
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const recipeId = req.params.id;
+
+    const { name, img, ingredients, steps } = req.body;
+    const updatedRecipe = await recipeService.update(
+        recipeId,
+        {
+            name,
+            imageUrl: img,
+            ingredients,
+            steps,
+        },
+        req.session.user.id
+    );
+
+    if (updatedRecipe) {
+        res.redirect('/recipes/details/' + updatedRecipe.id);
+    } else {
+        res.redirect('/users/login');
+    }
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const recipe = await recipeService.getOneById(req.params.id);
+
+    if (recipe.author != req.session.user.id) {
+        return res.redirect('/users/login');
+    }
+
+    recipe.ingredients = recipe.ingredients.join('\r\n');
+    recipe.steps = recipe.steps.join('\r\n');
+
+    res.render('delete', { recipe });
+});
+
+router.post('/delete/:id', async (req, res) => {
+    const recipeId = req.params.id;
+
+    try {
+        const isDeleted = await recipeService.deleteById(
+            recipeId,
+            req.session.user.id
+        );
+
+        if (isDeleted) {
+            res.redirect('/');
+        } else {
+            res.redirect('/users/login');
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.redirect('/recipes/details/' + recipeId);
+    }
+});
+
+module.exports = router;
--- routes.js
+++ routes.js
@@ -1,22 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
-const { isUser } = require('./services/util');
-
 const homeController = require('./controllers/home');
-const catalogController = require('./controllers/catalog');
-const detailsController = require('./controllers/details');
-const createController = require('./controllers/create');
-const editController = require('./controllers/edit');
 const authController = require('./controllers/auth');
-const deleteController = require('./controllers/delete');
+const recipeController = require('./controllers/recipe');
 
 router.use('/', homeController);
-router.use('/catalog', catalogController);
-router.use('/details', detailsController);
-router.use('/create', isUser(), createController);
-router.use('/edit', isUser(), editController);
-router.use('/delete', isUser(), deleteController);
+router.use('/recipes', recipeController);
 router.use('/users', authController);
 
 module.exports = router;
